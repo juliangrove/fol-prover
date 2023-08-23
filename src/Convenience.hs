4a3382fd4d5b695c5346154a4c0d34290e7948b1
juliangrove <julian.grove@gmail.com>
2023-08-23 13:21:11 -0400

    Minor.

--- src/Convenience.hs
+++ src/Convenience.hs
@@ -10,3 +10,15 @@ depth n fs = all (== Nothing) $ iterate loop [Just fs] !! n
 
 entails :: Int -> [Formula] -> Formula -> Bool
 entails n fs f = depth n $ (f, False) : map (, True) fs
+
+-- | Messing around:
+
+transitivity = Forall (Var 0) (Forall (Var 1) (Forall (Var 2) (Or (Not (And (Pred 0 [V (Var 0), V (Var 1)]) (Pred 0 [V (Var 1), V (Var 2)]))) (Pred 0 [V (Var 0), V (Var 2)]))))
+
+hyp n0 n1 = Pred 0 [N n0, N n1]
+
+hyp1 = hyp (Name 0) (Name 1)
+hyp2 = hyp (Name 1) (Name 2)
+hyp3 = hyp (Name 2) (Name 3)
+hyp4 = hyp (Name 0) (Name 2)
+
