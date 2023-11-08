a6759496d9aa9f051667f6a581ef5dfd201388b8
juliangrove <julian.grove@gmail.com>
2023-11-07 22:46:49 -0500

    Update rule lifting schema.

--- src/Tableaux.hs
+++ src/Tableaux.hs
@@ -8,7 +8,7 @@ type Signed = (Formula, Bool)
 type Tableau = [Maybe [Signed]]
 
 type PropositionalRule = Signed -> [[Signed]]
-type QuantifierRule = [Signed] -> Signed -> [Signed]
+type Rule = [Signed] -> PropositionalRule
 
 andRule, orRule, notRule :: PropositionalRule
 andRule sf@(And f0 f1, True) = [[(f0, True), (f1, True), sf]]
@@ -21,11 +21,11 @@ notRule sf@(Not f, True) = [[(f, False), sf]]
 notRule sf@(Not f, False) = [[(f, True), sf]]
 notRule sf = [[sf]]
 
-gammaRule, deltaRule :: QuantifierRule
+gammaRule, deltaRule :: Rule
 gammaRule fs sf = case sf of
-  (Forall v f0, True) -> [(subst v (N (nameT v f0)) f0, True), sf]
-  (Exists v f0, False) -> [(subst v (N (nameF v f0)) f0, False), sf]
-  _ -> [sf]
+  (Forall v f0, True) -> [[(subst v (N (nameT v f0)) f0, True), sf]]
+  (Exists v f0, False) -> [[(subst v (N (nameF v f0)) f0, False), sf]]
+  _ -> [[sf]]
   where nameT v f0 = name 0
           where name i = if (subst v (N (Name i)) f0, True) `elem` fs
                          then name (i + 1)
@@ -35,34 +35,29 @@ gammaRule fs sf = case sf of
                            then name (i + 1)
                            else Name i
 deltaRule fs = \case
-  (Exists v f0, True) -> [(subst v (N name) f0, True)]
-  (Forall v f0, False) -> [(subst v (N name) f0, False)]
-  sf -> [sf]
+  (Exists v f0, True) -> [[(subst v (N name) f0, True)]]
+  (Forall v f0, False) -> [[(subst v (N name) f0, False)]]
+  sf -> [[sf]]
   where name = case concatMap (names . fst) fs of
                  [] -> Name 0
                  ns -> Name (i + 1)
                    where Name i = maximum ns
 
-applyRule :: PropositionalRule -> [Signed] -> [[Signed]]
-applyRule _ [(Top, True)] = [[(Top, True)]]
-applyRule _ [] = [[(Top, True)]]
-applyRule rule (f:fs) = applyRule rule fs >>= justTheFirst
-  where justTheFirst :: [Signed] -> [[Signed]]
-        justTheFirst g = map (++ g) $ rule f
-
-applyQuantRule :: QuantifierRule -> [Signed] -> [Signed]
-applyQuantRule rule fs = applyQuantRule0 rule fs [(Top, True)]
-  where applyQuantRule0 _ [(Top, True)] fs' = fs'
-        applyQuantRule0 _ [] fs' = fs'
-        applyQuantRule0 rule fs1@(f:fs0) fs' =
-          applyQuantRule0 rule fs0 (rule (fs1 ++ fs') f ++ fs')
+applyRule :: Rule -> [Signed] -> [[Signed]]
+applyRule rule p = applyRule' rule p [[(Top, True)]]
+  where applyRule' _ [] t = t
+        applyRule' _ [(Top, True)] t = t
+        applyRule' rule (f:fs) t = applyRule' rule fs t'
+          where t' = do p <- t
+                        b <- rule (f:p ++ fs) f
+                        pure (p ++ b)
 
 allRules :: [Signed] -> [[Signed]]
-allRules fs = do fs <- applyRule orRule fs
-                 fs <- applyRule andRule fs
-                 fs <- applyRule notRule fs
-                 fs <- pure (applyQuantRule gammaRule fs)
-                 fs <- pure (applyQuantRule deltaRule fs)
+allRules fs = do fs <- applyRule (const orRule) fs
+                 fs <- applyRule (const andRule) fs
+                 fs <- applyRule (const notRule) fs
+                 fs <- applyRule gammaRule fs
+                 fs <- applyRule deltaRule fs
                  pure fs
 
 contradictory :: [Signed] -> Bool
